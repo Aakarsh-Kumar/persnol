2ad2a188e5b8b325fa452cc7c1198fbd948c77ec
Aakarsh-Kumar <aakarsh2504@gmail.com>
2021-09-09 21:16:54 +0530

    first commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, request, url_for
 
 app = Flask(__name__,template_folder='templates')
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def index():
     return render_template('index.html')
 
