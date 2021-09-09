cc097adc03649350406979b488055033a9121e68
Aakarsh-Kumar <aakarsh2504@gmail.com>
2021-09-09 21:00:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, redirect, request, url_for
+# import mysql.connector
+
+app = Flask(__name__,template_folder='templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/portfolio-details')
+def portfolioDetails():
+    return render_template('portfolio-details.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
